10e993ff98938bd887bcd23988960778debe843d
Taras.Kravets <taraskravets95@gmail.com>
2023-02-02 14:46:10 +0200

    feat(hint): added hint to InfoPage

---
+++ src/components/Hint.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useAppSelector } from '../store/hooks'
+import IDescription from '../interfaces/description'
+
+interface IHint {
+	id: number
+}
+
+const Hint: React.FC<IHint> = ({ id }) => {
+	const descriptionList = useAppSelector<IDescription[]>(
+		state => state.descriptionList.descriptionList,
+	)
+
+	const correctElem = descriptionList.find(
+		(element: IDescription) => element.ID === id,
+	)
+	return (
+		<div className="hint">
+			{correctElem ? (
+				<p
+					dangerouslySetInnerHTML={{
+						__html: correctElem.Description,
+					}}
+				/>
+			) : (
+				// eslint-disable-next-line react/no-unescaped-entities
+				<p className="hint-error">Can't find this description</p>
+			)}
+		</div>
+	)
+}
+
+export default Hint
--- src/pages/InfoPage.tsx
+++ src/pages/InfoPage.tsx
@@ -3,9 +3,13 @@ import axios from 'axios'
 import IVehicleVariableByVin from '../interfaces/vehicleVariablesByVin'
 import { useAppSelector } from '../store/hooks'
 
+import Hint from '../components/Hint'
+
 const InfoPage = () => {
 	const [fetch, setFetch] = useState<IVehicleVariableByVin[]>([])
+	const [showHintByID, setShowHintByID] = useState<number | null>(null)
 	const vinList = useAppSelector<string[] | []>(state => state.searchList)
+
 	useEffect(() => {
 		const fetchVIN = async () => {
 			try {
@@ -28,10 +32,21 @@ const InfoPage = () => {
 			{fetch.length > 0 &&
 				fetch?.map((el: IVehicleVariableByVin) => (
 					<div key={el?.VariableId} style={{ marginBottom: '10px' }}>
-						{el.Value && (
+						{el.Value && el.Value !== 'Not Applicable' && (
 							<>
-								<p>{el?.Variable}</p>
-								<p>{el?.Value}</p>
+								<div
+									onMouseEnter={() =>
+										setShowHintByID(el.VariableId)
+									}
+									onMouseLeave={() => setShowHintByID(null)}
+								>
+									<p>{el?.Variable}</p>
+									<p>{el?.Value}</p>
+								</div>
+								{showHintByID &&
+									showHintByID === el.VariableId && (
+										<Hint id={showHintByID} />
+									)}
 							</>
 						)}
 					</div>
