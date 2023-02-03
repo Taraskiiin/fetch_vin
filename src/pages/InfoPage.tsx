1fa660bc6afdd1dfd9ebbdc79ab554d48c184dfd
Taras.Kravets <taraskravets95@gmail.com>
2023-02-03 14:53:06 +0200

    fix(UI): fixed jumping el.Value

--- src/pages/InfoPage.tsx
+++ src/pages/InfoPage.tsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
 import IVehicleVariableByVin from '../interfaces/vehicleVariablesByVin'
 import { useAppSelector } from '../store/hooks'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLongArrowAltLeft, faMinus } from '@fortawesome/free-solid-svg-icons'
 
 import Hint from '../components/Hint'
 
@@ -31,6 +34,9 @@ const InfoPage = () => {
 
 	return (
 		<div className="list">
+			<Link to="/">
+				<FontAwesomeIcon icon={faLongArrowAltLeft} />
+			</Link>
 			{fetch.length > 0 &&
 				fetch?.map((el: IVehicleVariableByVin) => (
 					<>
@@ -42,7 +48,10 @@ const InfoPage = () => {
 										setShowHintByID(el.VariableId)
 									}
 									onMouseLeave={() => setShowHintByID(null)}>
-									<p>{el?.Variable} :</p>
+									<p>
+										{el?.Variable}{' '}
+										<FontAwesomeIcon icon={faMinus} />
+									</p>
 									<p>{el?.Value}</p>
 									{showHintByID &&
 										showHintByID === el.VariableId && (
